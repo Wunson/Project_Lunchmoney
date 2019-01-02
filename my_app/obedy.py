88780486fd4492f89b801b7703fc522382f949e7
Ondřej Wünsch <wunsch.ondrej@gmail.com>
2019-01-02 13:08:24 +0100

    píp

--- my_app/obedy.py
+++ my_app/obedy.py
@@ -1,27 +1,22 @@
 from flask import Flask
 from flask import render_template
+import threading
+
+stravnici = []
+
+def pip():
+    global stravnici
+    while 1:
+        stravnici.append(input(">>"))
+        if len(stravnici) > 4:
+            stravnici.remove(stravnici[0])
+
+threading._start_new_thread(pip, ())
 
 app = Flask(__name__)
 
 @app.route("/")
 @app.route("/index")
 def index():
-    stravnici = [
-    {"jmeno" : "Jakub Vašíček",
-    "trida" : "4B",
-    "obed" : "n",
-    "rozvrh" : True
-    },
-    {"jmeno" : "Kryštof Stejskal",
-    "trida" : "4B",
-    "obed" : "1",
-    "rozvrh" : True
-    },
-    {"jmeno" : "Lucie Čechová",
-    "trida" : "3B",
-    "obed" : "2",
-    "rozvrh" : False
-    }
-    ]
-
-    return render_template("index.html", stravnici = stravnici)
+    global stravnici
+    return render_template("test.html", stravnici = stravnici)
