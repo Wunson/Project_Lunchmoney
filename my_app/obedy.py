7ece7d0b9bb84d3f957cc997f6d5a711d30cfcd6
Ondřej Wünsch <wunsch.ondrej@gmail.com>
2019-01-28 13:27:59 +0100

    dtaase

--- my_app/obedy.py
+++ my_app/obedy.py
@@ -1,13 +1,22 @@
 from flask import Flask
 from flask import render_template
 import threading
+import mysql.connector
+
+mydb =  mysql.connector.connect(host="localhost",user= "root", passwd= "heslo", db = "obedy_test")
+cur = mydb.cursor()
 
 stravnici = []
 
 def pip():
     global stravnici
     while 1:
-        stravnici.append(input(">>"))
+        karta = input(">>")
+        dotaz = f"SELECT * FROM OBEDY WHERE id={karta} "
+        cur.execute(dotaz)
+        result = cur.fetchall()
+
+        stravnici.append(result[0][4])
         if len(stravnici) > 4:
             stravnici.remove(stravnici[0])
 
