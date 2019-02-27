986167e933f55fc10b2226cda3c2e9811d681af8
Ondřej Wünsch <wunsch.ondrej@gmail.com>
2019-02-27 10:25:45 +0100

    fixed

--- obedy_test.py
+++ obedy_test.py
@@ -1,51 +1,74 @@
 from flask import Flask, jsonify, render_template
 import threading
+import logging
 import mysql.connector
 
-mydb = mysql.connector.connect(host="localhost", \
-                                user="root", \
-                                passwd="heslo", \
+mydb = mysql.connector.connect(host="localhost",
+                                user="root",
+                                passwd="heslo",
                                 db="obedy_test")
 cursor = mydb.cursor()
 
 def kolik_obedu():
-    cursor.execute("SELECT COUNT(*) FROM OBEDY WHERE cislo_obedu=1")
+    cursor.execute("SELECT COUNT(*) FROM OBEDY WHERE cislo_obedu=1 AND stav=1")
     jednicky = cursor.fetchall()
-    cursor.execute("SELECT COUNT(*) FROM OBEDY WHERE cislo_obedu=2")
+    cursor.execute("SELECT COUNT(*) FROM OBEDY WHERE cislo_obedu=2 AND stav=1")
     dvojky = cursor.fetchall()
-
     return {"jednicky":jednicky[0][0], "dvojky":dvojky[0][0]}
 
-def pip():
+def vydat_obed(karta, stravnik):
+    global pocty_obedu
+    if stravnik[2] == 1:
+        pocty_obedu["jednicky"] -=1
+    elif stravnik[2] == 2:
+        pocty_obedu["dvojky"] -=1
+    else:
+        pass
+
+    cursor.execute("UPDATE OBEDY SET STAV=2 WHERE id=%s"% (karta)) #MÍSTO ID DÁT DATA Z KARTY
+
+def pip(karta):
+    cursor.execute("SELECT * FROM OBEDY WHERE id=%s"% (karta)) #MÍSTO ID DÁT DATA Z KARTY
+    result = cursor.fetchall()
+    print(result)
+    return (result[0][4], result[0][5], result[0][6], result[0][7])
+
+def reader_loop():
     global stravnici
-    global obedy
-    stravnici = []
     while 1:
-        karta = input(">>")
+        karta = input(">>")     #SEM DÁT ČTENÍ KARTY
         if karta == "q":
             break
         if karta:
-            cursor.execute(f"SELECT * FROM OBEDY WHERE id={karta}")
-            result = cursor.fetchall()
-
-            stravnik = (result[0][4], result[0][5], result[0][6])
+            stravnik = pip(karta)
+            print(stravnik, "pip")
+            print(stravnici)
             stravnici.append(stravnik)
-            if len(stravnici) > 4:
-                stravnici.remove(stravnici[0])
+            print(stravnici)
+            print(len(stravnici))
 
-            cursor.execute(f"UPDATE OBEDY SET STAV=2 WHERE ID={karta}")
+            #if len(stravnici) > 4:
+            #    stravnici.remove(stravnici[0])
 
-            if stravnik[2] == 1:
-                obedy["jednicky"] -=1
-            elif stravnik[2] == 2:
-                obedy["dvojky"] -=1
+            if stravnik[3] == 1:
+                print(pocty_obedu, "vydani")
+                vydat_obed(karta, stravnik)
+                print(pocty_obedu)
+                print (stravnici)
+            else:
+                pass
 
-obedy = kolik_obedu()
-threading._start_new_thread(pip, ())
+stravnici = []
+print(stravnici, "generate")
+pocty_obedu = kolik_obedu()
+threading._start_new_thread(reader_loop, ())
 
 
 
 app = Flask(__name__)
+log = logging.getLogger('werkzeug')
+log.disabled = True
+app.logger.disabled = True
 
 @app.route("/")
 @app.route("/index")
@@ -54,4 +77,5 @@ def index():
 
 @app.route("/get_data", methods=["POST", "GET"])
 def send_data():
-    return jsonify(stravnici, obedy)
+    print(stravnici, pocty_obedu, "sent")
+    return jsonify(stravnici, pocty_obedu)
