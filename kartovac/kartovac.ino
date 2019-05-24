d6caf1897db585b2dcd362c26fc3bcdf9b7714cb
Ondřej Wünsch <wunsch.ondrej@gmail.com>
2019-05-24 07:01:58 +0200

    done

--- kartovac/kartovac.ino
+++ kartovac/kartovac.ino
@@ -1,9 +1,9 @@
 void setup() {
-  char cards[4][16] = {"127FB2B6", "E47F410E", "ACFA5B47", "A455420E"};
+  char cards[5][16] = {"127FB2B6", "E47F410E", "ACFA5B47", "A455420E", "F458430E"};;
 
   Serial.begin(9600);
   randomSeed(analogRead(A0));
-  Serial.println(cards[random(0,3)]);
+  Serial.println(cards[random(0,5)]);
 }
 
 void loop() {
